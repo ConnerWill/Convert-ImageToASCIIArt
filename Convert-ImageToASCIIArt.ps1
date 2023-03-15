4986d8aa7552292b9ccee5614f7be77cc66e754e
Conner.Will <conner.will@infinitecampus.com>
2023-03-15 09:11:21 -0500

    Added descriptions to examples in help section

--- Convert-ImageToASCIIArt.ps1
+++ Convert-ImageToASCIIArt.ps1
@@ -12,12 +12,19 @@ function Convert-ImageToASCIIArt {
       Convert-ImageToAsciiArt -ImagePath "C:\path\to\image.jpg"
 
       Example of converting image 'C:\path\to\image.jpg' to ASCII art
+
   .EXAMPLE
       Convert-ImageToAsciiArt -ImagePath "C:\path\to\image.jpg" -MaxWidth 80 -Contrast 75
 
       Example of converting image 'C:\path\to\image.jpg' to ASCII art,
       specifying the MaxWidth of ASCII art output to 80 instead of default value of 120
       and the Contrast to 75 instead of the default value of 50
+
+  .EXAMPLE
+      Convert-ImageToAsciiArt C:\path\to\image.jpg
+
+      Example of converting image 'C:\path\to\image.jpg' to ASCII art without using the 'ImagePath' flag
+      This flag is not needed if the image path is specified as the first value when calling the function
 #>
     Param (
         [Parameter(
